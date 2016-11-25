0f4b33d0ced1598c63fff54bf97b06761bb06133
gurayyildirim <guray@gurayyildirim.com.tr>
2016-11-25 16:58:53 +0300

    Initial files added.

---
+++ main.py
@@ -0,0 +1,34 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+import paho.mqtt.client as mqtt
+
+
+class RootWidget(BoxLayout):
+	"""docstring for RootWidget"""
+	def __init__(self, **kwargs):
+		super(RootWidget, self).__init__(**kwargs)
+		self.client = mqtt.Client()
+		self.client.on_connect = self.on_connect
+		self.client.on_message = self.on_message
+		self.client.connect("gu.ray.kim", 1883, 60)
+		self.client.loop_start()
+
+        def on_message(self, client, userdata, msg):
+		self.ids.temp.text = str(msg.payload) + " C"
+
+	def on_connect(self, client, userdata, flags, rc):
+		self.client.subscribe("/devfest/temp")
+
+	def stateChanged(self, newState):
+		self.client.publish("/devfest", int(newState))
+		
+
+class DevFestApp(App):
+	"""docstring for DevfestApp"""
+	def build(self):
+		return RootWidget()
+		
+
+if __name__ == '__main__':
+	DevFestApp().run()
